1555dfe5842762fbcda80b075db3218e80d15544
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-03-02 10:39:11 +0530

    created database

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-echo "hello php";
+include("./config.php");
+
+$dbname = "school";
+$createDatabaseQuery = "CREATE DATABASE $dbname";
+
+$stmt = $conn->prepare($createDatabaseQuery);
+if($stmt){
+    if($stmt->execute()){
+        echo "database $dbname created successfully";
+    }else{
+        die("error while creating database".$conn->error);
+    }
+}else{
+    die("Query preparation failed".$conn->error);
+}
+
 ?>
